d7699cc9ed0cf3f743acc322d478b7a9746decb7
muraleo <muraleo80923@gmail.com>
2022-07-08 11:39:16 -0400

    create section header

---
+++ src/elements/SectionHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionHeader = ({header}) => {
+    return (
+        <div className="section-header">
+            {header}
+        </div>
+    );
+};
+
+export default SectionHeader;
