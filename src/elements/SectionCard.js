adc40aee4ae1c2e4315d62c593be9dee081cd40e
muraleo <muraleo80923@gmail.com>
2022-07-08 21:07:32 -0400

    add section card highlight

--- src/elements/SectionCard.js
+++ src/elements/SectionCard.js
@@ -3,9 +3,9 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CountDownCircle from "../elements/CountDownCircle";
 
-const SectionCard = ({ content, countDownTime }) => {
+const SectionCard = ({ content, countDownTime, highlight }) => {
   return (
-    <Card className="section-card">
+    <Card className={`section-card ${highlight ? "highlight" : ""}`}>
       <CardContent className="flex items-center">
         {countDownTime && <CountDownCircle countDownTime={countDownTime} />}
         <div>{content}</div>
